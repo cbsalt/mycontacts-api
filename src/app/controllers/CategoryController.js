87272333b602cc905e0eea8168d027fe495db465
Charles Sal <charlessal@Charless-MacBook-Pro.local>
2021-08-25 17:09:32 -0300

    feat: routes, controllers, repository pattern

---
+++ src/app/controllers/CategoryController.js
@@ -0,0 +1,36 @@
+const CategoriesRepository = require('../repositories/CategoriesRepository');
+
+class CategoryController {
+  async index(request, response) {
+    const { orderBy } = request.query;
+    const categories = await CategoriesRepository.findAll(orderBy);
+
+    response.json(categories);
+  }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const category = await CategoriesRepository.findById(id);
+
+    if (!category) {
+      return response.status(404).json({ error: 'Category not found' });
+    }
+
+    response.json(category);
+  }
+
+  async store(request, response) {
+    const { name } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const category = await CategoriesRepository.create({ name });
+
+    response.json(category);
+  }
+}
+
+module.exports = new CategoryController();
